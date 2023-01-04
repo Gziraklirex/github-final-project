76622eb84917fabf49dd069cc3b141e8c8ba1307
Irakli-GZ <121023107+Irakli-GZ@users.noreply.github.com>
2023-01-04 18:52:39 +0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -6,13 +6,13 @@
 
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Irakli-GZ
 
    # Input:
    # p, principal amount
    # t, time period in years
    # r, annual rate of interest
-
+   
    # Output:
    # simple interest = p*t*r
 
